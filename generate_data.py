81c94a6b5952acb001c51636f4ba77c08253113c
Rauan Akylzhanov <r.akylzhanov14@imperial.ac.uk>
2018-06-29 12:58:25 +0100

    Data ready to feed and the successful model creation

--- generate_data.py
+++ generate_data.py
@@ -1,16 +1,13 @@
 from sympy import symbols
 from sympy.polys.specialpolys import symmetric_poly
+
 import random
 import csv
 import pandas as pd
+import tensorflow as tf
+
 
 def generate_csv(filename, gens, polynoms, values, a,b):
-
-	# lines 18-34 below get values ready to write to csv file
-	# getting highest degree of polynomials in polynoms
-	d = len(gens)
-	# print("d={}".format(d))
-	n = len(polynoms)
 
 	csvfile = open(filename,"w")
 	fieldnames = values[0].keys()
@@ -33,10 +30,10 @@ def load_csv(filename):
 	# print("CSV_COLUMNS={}".format(CSV_COLUMNS))
 
 	data_features, data_label = data, data.pop('p')
-	print("train features")
-	print(data_features)
-	print("train label")
-	print(data_label)
+	# print("train features")
+	# print(data_features)
+	# print("train label")
+	# print(data_label)
    # train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
 	return data_features, data_label
 
@@ -45,28 +42,18 @@ def load_csv(filename):
 
 def generate_polynoms(d, n):
 	polynoms = []
-# setting dimension
-
 	gens=[]
 	for i in range(1,d+1):
 		# print("i={}".format(i))
 		x_i=symbols("x"+str(i))
 		gens.append(x_i)
-	# print("gens = {}".format(gens))
-# print("len(gens)={}".format(len(gens)))
-# generating list of n polynomials with x1,x2,...,xd arguments
-# the degrees are between 1 and d
-	# for _ in range(n):
-
 	for i in range(1,d+1):
 		polynoms.append(symmetric_poly(i,gens))
-
 	return polynoms, gens
-
-def generate_values(polynoms,gens, n):
 # setting random values to arguments in gens
 # evaluating polynomial values
 # number of different argument values
+def generate_values(polynoms,gens, n):
 	d = len(gens)
 	values={}
 	for i in range(n):
@@ -83,3 +70,16 @@ def generate_values(polynoms,gens, n):
 		values[i]=value
 
 	return values
+
+def train_input_fn(features, labels, batch_size):
+	"""An input function for training"""
+	# Convert the inputs to a Dataset.
+	dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+	print("features={}".format(features))
+	print("dict(features)={}".format(dict(features)))
+	print("labels={}".format(labels))
+	# Shuffle, repeat, and batch the examples.
+	dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+	# Return the dataset.
+	return dataset
--- my_estimator.py
+++ my_estimator.py
@@ -1,12 +1,15 @@
+#!/anaconda3/envs/tensorflow/bin/python
+import tensorflow as tf
 import generate_data
+import argparse
 # we transform data to feed into model
 def parse_csv(line):
 	example_defaults = [[0], [0], [0], [0], [0]]  # sets field types
 	column_names = ['x1','x2','x4','x5','p']
 	feature_names = column_names[:-1]
 	label_name = column_names[-1]
-	print("feature_names={}".format(feature_names))
-	print("label_names={}".format(feature_names))
+	# print("feature_names={}".format(feature_names))
+	# print("label_names={}".format(feature_names))
 
 	print("Features: {}".format(feature_names))
 	print("Label: {}".format(label_name))
@@ -17,8 +20,8 @@ def parse_csv(line):
   # label = tf.reshape(parsed_line[-1], shape=())
 	# return features, label
 
-if __name__=="__main__":
 # 0) Generate and save data: lines 103-105
+
 # 1) Import and parse the data sets: lines 106-108
 # 2)Create feature columns to describe the data.
 # 3)Select the type of model
@@ -26,6 +29,14 @@ if __name__=="__main__":
 # 5)Evaluate the model's effectiveness.
 # 6)Let the trained model make predictions.
 
+def main(argv):
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--batch_size', default=100, type=int, help='batch size')
+	parser.add_argument('--train_steps', default=1000, type=int,
+					help='number of training steps')
+	args = parser.parse_args(argv[1:])
+	# print("args={}".format(args))
+	# Fetch the data
 	polynoms,gens = generate_data.generate_polynoms(4,2)
 	values   = generate_data.generate_values(polynoms,gens, 34)
 	# 0..34 = 0..24 and 25..24
@@ -33,10 +44,59 @@ if __name__=="__main__":
 	generate_data.generate_csv("test.csv",gens, polynoms,values,25,34)
 	train_features, train_label = generate_data.load_csv("./train.csv")
 	test_features, test_label = generate_data.load_csv("./test.csv")
+	# Feature columns describe how to use the input.
 	my_feature_columns = []
 	for key in train_features.keys():
-		print("train_key={}".format(key))
-	for key in test_features.keys():
-		print("test_key={}".format(key))
+		my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+		# print("train_key={}".format(key))
+
+# Build 2 hidden layer DNN with 10, 10 units respectively.
+	classifier = tf.estimator.DNNClassifier(
+		feature_columns=my_feature_columns,
+		# Two hidden layers of 10 nodes each.
+		hidden_units=[10, 10],
+		# The model must choose between 3 classes.
+		n_classes=3)
+
+	# Train the Model.
+	# classifier.train(
+	# 	input_fn=lambda:iris_data.train_input_fn(train_x, train_y,
+	# 											 args.batch_size),
+	# 	steps=args.train_steps)
+
+	# Evaluate the model.
+	# eval_result = classifier.evaluate(
+	# 	input_fn=lambda:iris_data.eval_input_fn(test_x, test_y,
+	# 											args.batch_size))
+
+	# print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+	#
+	# # Generate predictions from the model
+	# expected = ['Setosa', 'Versicolor', 'Virginica']
+	# predict_x = {
+	# 	'SepalLength': [5.1, 5.9, 6.9],
+	# 	'SepalWidth': [3.3, 3.0, 3.1],
+	# 	'PetalLength': [1.7, 4.2, 5.4],
+	# 	'PetalWidth': [0.5, 1.5, 2.1],
+	# }
+	#
+	# predictions = classifier.predict(
+	# 	input_fn=lambda:iris_data.eval_input_fn(predict_x,
+	# 											labels=None,
+	# 											batch_size=args.batch_size))
+	#
+	# template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
+	#
+	# for pred_dict, expec in zip(predictions, expected):
+	# 	class_id = pred_dict['class_ids'][0]
+	# 	probability = pred_dict['probabilities'][class_id]
+	#
+	# 	print(template.format(iris_data.SPECIES[class_id],
+	# 						  100 * probability, expec))
+
+if __name__ == '__main__':
+	tf.logging.set_verbosity(tf.logging.INFO)
+	tf.app.run(main)
+
 
 #
